cc95f50e3c1022985416cd1bf0426ef566fc83b0
dryErase <beno695@gmail.com>
2018-11-03 04:05:14 -0700

    Initial Commit

---
+++ calhacks_receipt_split/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calhacks_receipt_split
+//
+//  Created by Benjamin Pust on 11/3/18.
+//  Copyright © 2018 Benjamin Pust. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
